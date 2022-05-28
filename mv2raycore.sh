846cccc3af558a35c77619d7a251a7ffb908e20b
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-05-29 01:37:31 +0800

    Update mv2raycore.sh

--- mv2raycore.sh
+++ mv2raycore.sh
@@ -4,16 +4,16 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 
-echo -e "${green}PANEL V2RAY CORE${NC}"
+echo -e "${red}PANEL V2RAY CORE${NC}"
 echo -e "=========================================================================="
-echo -e "${green}V2RAY VMESS${NC}"
+echo -e "${red}V2RAY VMESS${NC}"
 echo -e "=========================================================================="
 echo -e "[ 1 ] CREATE NEW USER"
 echo -e "[ 2 ] DELETE ACTIVE USER"
 echo -e "[ 3 ] EXTEND ACTIVE USER"
 echo -e "[ 4 ] CHECK USER LOGIN"
 echo -e "=========================================================================="
-echo -e "${green}V2RAY VLESS${NC}"
+echo -e "${red}V2RAY VLESS${NC}"
 echo -e "=========================================================================="
 echo -e "[ 5 ] CREATE NEW USER"
 echo -e "[ 6 ] DELETE ACTIVE USER"
