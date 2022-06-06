506bff94cbf112072ed96d635fe7f33bf1dcd5ed
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-06-07 02:54:16 +0800

    Add files via upload

--- jinggo.sh
+++ jinggo.sh
@@ -6,27 +6,17 @@ blue='\e[0;34m'
 purple='\e[1;35m'
 blue_b='\e[0;94m'
 NC='\e[0m'
-echo -e  " "
-echo -e  " "
-echo -e  "${blue_b}        _nnnn_ "                    
-echo -e  "${blue_b}       dGGGGMMb     ,"""""""""""""". "
-echo -e  "${blue_b}      @p~qp~~qMb    |   WELCOME !  | "
-echo -e  "${blue_b}      M|@||@) M|   _;..............' "
-echo -e  "${blue_b}      @,----.JM| -'   ░█▀▀█ ░█▀▀▀ ░█──░█ ░█▀▀▀█ ── ░█──░█ ─█▀█─ "${NC}
-echo -e  "${blue_b}     JS^\__/  qKL     ░█▄▄▀ ░█▀▀▀ ░█▄▄▄█ ─▄▄▄▀▀ ▀▀ ─░█░█─ █▄▄█▄  "${NC}
-echo -e  "${blue_b}   dZP        qKRb    ░█─░█ ░█▄▄▄ ──░█── ░█▄▄▄█ ── ──▀▄▀─ ───█─   "${NC}
-echo -e  "${blue_b}   dZP          qKKb "
-echo -e  "${blue_b}  fZP            SMMb      ░█▀▀▀█ ░█▀▀█ ░█▀▀█ ▀█▀ ░█▀▀█ ▀▀█▀▀  "${NC}
-echo -e  "${blue_b}  HZM            MMMM      ─▀▀▀▄▄ ░█─── ░█▄▄▀ ░█─ ░█▄▄█ ─░█──    "${NC}          
-echo -e  "${blue_b}  FqM            MMMM      ░█▄▄▄█ ░█▄▄█ ░█─░█ ▄█▄ ░█─── ─░█──     "${NC}
-echo -e  "${blue_b}  __| '.        |\dS*qML   "
-echo -e  "${blue_b} |    `.       | `' \Zq    "
-echo -e  "${blue_b} _)      \.___.,|     .'   "
-echo -e  "${blue_b}  \____   )MMMMMM|   .'    "
-echo -e  "${blue_b}      `-'       `--' Premium Script  "
 
+
+echo -e  "${blue_b}      ░█▀▀█ ░█▀▀▀ ░█──░█ ░█▀▀▀█ ── ░█──░█ ─█▀█─      "${NC}      
+echo -e  "${blue_b}      ░█▄▄▀ ░█▀▀▀ ░█▄▄▄█ ─▄▄▄▀▀ ▀▀ ─░█░█─ █▄▄█▄        "${NC}
+echo -e  "${blue_b}      ░█─░█ ░█▄▄▄ ──░█── ░█▄▄▄█ ── ──▀▄▀─ ───█─          "${NC}
 echo -e  ""
-echo -e  "${red}JANGAN CAKAP ABANG TAK PAYUNG ! ERROR ? CONTACT TELEGRAM @GHReyz"
+echo -e  "${blue_b}         ░█▀▀▀█ ░█▀▀█ ░█▀▀█ ▀█▀ ░█▀▀█ ▀▀█▀▀           "${NC}            
+echo -e  "${blue_b}         ─▀▀▀▄▄ ░█─── ░█▄▄▀ ░█─ ░█▄▄█ ─░█──             "${NC}
+echo -e  "${blue_b}         ░█▄▄▄█ ░█▄▄█ ░█─░█ ▄█▄ ░█─── ─░█──              "${NC}
+echo -e  ""
+echo -e  "${purple}JANGAN CAKAP ABANG TAK PAYUNG ! ERROR ? CONTACT TELEGRAM @GHReyz"
 echo -e  ""
 echo -e  "${red}TYPE 'menu' TO GO TO MAIN OR 'exit' TO LOGOUT"${NC}
 echo -e  ""
