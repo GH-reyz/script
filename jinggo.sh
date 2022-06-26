df4f7178f0711a8057244b6e1222ad3ad0af0038
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-06-27 03:20:19 +0800

    Update jinggo.sh

--- jinggo.sh
+++ jinggo.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ini#!/bin/bash
 clear
 red='\e[1;31m'
 green='\e[0;32m'
@@ -11,14 +11,14 @@ echo -e "#====================================================================#"
 echo -e "#                   Premium Script By REYZ-V4                        #" | lolcat
 echo -e "#                                                                    #" | lolcat
 echo -e "#                                                                    #" | lolcat
-echo -e "#        ░█▀▀█ █▀▀ █──█ ▀▀█ ── ░█──░█ ─█▀█─                          #" | lolcat
-echo -e "#        ░█▄▄▀ █▀▀ █▄▄█ ▄▀─ ▀▀ ─░█░█─ █▄▄█▄                          #" | lolcat
-echo -e "#        ░█─░█ ▀▀▀ ▄▄▄█ ▀▀▀ ── ──▀▄▀─ ───█─                          #" | lolcat
+echo -e "#           ░█▀▀█ █▀▀ █──█ ▀▀█ ── ░█──░█ ─█▀█─                       #" | lolcat
+echo -e "#           ░█▄▄▀ █▀▀ █▄▄█ ▄▀─ ▀▀ ─░█░█─ █▄▄█▄                       #" | lolcat
+echo -e "#           ░█─░█ ▀▀▀ ▄▄▄█ ▀▀▀ ── ──▀▄▀─ ───█─                       #" | lolcat
 echo -e "#                                                                    #" | lolcat
 echo -e "#                         - Script By REYZ-V4                        #" | lolcat
 echo -e "#           │▒│ /▒/       - Thanks To ALLAH SWT                      #" | lolcat
 echo -e "#           │▒│/▒/        - Whatsapp  : 🔏                           #" | lolcat
-echo -e "#         　│▒ /▒/─┬─┐    - Telegram : t.me/GHReyz                  #" | lolcat
+echo -e "#         　│▒ /▒/─┬─┐    - Telegram : t.me/GHReyz                   #" | lolcat
 echo -e "#           │▒│▒|▒│▒│     - To those who use this script,            #" | lolcat
 echo -e "#          ┌┴─┴─┐-┘─┘       sorry because this script very simple    #" | lolcat
 echo -e "#          │▒┌──┘▒▒▒│     - Thanks for who using this script         #" | lolcat
@@ -28,8 +28,8 @@ echo -e "#====================================================================#"
 echo -e ""
 echo -e "                         \e[1;31mCOPYRIGHT © 6/2004\e[0m "
 echo -e "               \e[1;31mReyzVPN POWERED BY GHReyz / Reyz-V4\e[0m"
+echo -e "          \e[1;31mPrivacy Policy Terms & Conditions Disclaimer\e[0m"
 echo -e " "
-echo -e "            \e[1;31mPrivacy Policy Terms & Conditions Disclaimer\e[0m"
 echo -e  " TYPE 'menu' TO GO TO MAIN OR 'exit' TO LOGOUT" | lolcat
 echo -e  ""
 
