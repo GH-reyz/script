c46aaedd3b07c90ed018e0fe5980c885198f5a8a
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-06-27 03:01:52 +0800

    Update jinggo.sh

--- jinggo.sh
+++ jinggo.sh
@@ -7,18 +7,28 @@ purple='\e[1;35m'
 blue_b='\e[0;94m'
 NC='\e[0m'
 
-
-echo -e  "${blue_b}  ░█▀▀█ ░█▀▀▀ ░█──░█ ░█▀▀▀█ ── ░█──░█ ─█▀█─      "${NC}      
-echo -e  "${blue_b}  ░█▄▄▀ ░█▀▀▀ ░█▄▄▄█ ─▄▄▄▀▀ ▀▀ ─░█░█─ █▄▄█▄        "${NC}
-echo -e  "${blue_b}  ░█─░█ ░█▄▄▄ ──░█── ░█▄▄▄█ ── ──▀▄▀─ ───█─          "${NC}
-echo -e  " "
-echo -e  " "
-echo -e  "${blue_b}    ░█▀▀▀█ ░█▀▀█ ░█▀▀█ ▀█▀ ░█▀▀█ ▀▀█▀▀           "${NC}            
-echo -e  "${blue_b}    ─▀▀▀▄▄ ░█─── ░█▄▄▀ ░█─ ░█▄▄█ ─░█──             "${NC}
-echo -e  "${blue_b}    ░█▄▄▄█ ░█▄▄█ ░█─░█ ▄█▄ ░█─── ─░█──              "${NC}
-echo -e  ""
-echo -e  " JANGAN CAKAP ABANG TAK PAYUNG ! ERROR ? CONTACT TELEGRAM @GHReyz" | lolcat
-echo -e  ""
+echo -e "#====================================================================#" | lolcat
+echo -e "#                   Premium Script By REYZ-V4                        #" | lolcat
+echo -e "#                                                                    #" | lolcat
+echo -e "#                                                                    #" | lolcat
+echo -e "#        ░█▀▀█ █▀▀ █──█ ▀▀█ ── ░█──░█ ─█▀█─                     #" | lolcat
+echo -e "#        ░█▄▄▀ █▀▀ █▄▄█ ▄▀─ ▀▀ ─░█░█─ █▄▄█▄                     #" | lolcat
+echo -e "#        ░█─░█ ▀▀▀ ▄▄▄█ ▀▀▀ ── ──▀▄▀─ ───█─                     #" | lolcat
+echo -e "#                                                                    #" | lolcat
+echo -e "#                         - Script By REYZ-V4                        #" | lolcat
+echo -e "#           │▒│ /▒/       - Thanks To ALLAH SWT                             #" | lolcat
+echo -e "#           │▒│/▒/        - Whatsapp  : 🔏                              #" | lolcat
+echo -e "#         　│▒ /▒/─┬─┐    - Telegram : t.me/GHReyz                 #" | lolcat
+echo -e "#           │▒│▒|▒│▒│     - To those who use this script,            #" | lolcat
+echo -e "#          ┌┴─┴─┐-┘─┘       sorry because this script very simple    #" | lolcat
+echo -e "#          │▒┌──┘▒▒▒│     - Thanks for who using this script         #" | lolcat
+echo -e "#          └┐▒▒▒▒▒▒┌┘     - To EnterMenuList = 'menu'                #" | lolcat
+echo -e "#                                                                    #" | lolcat
+echo -e "#====================================================================#" | lolcat
+echo -e ""
+echo -e "                         \e[1;31mCOPYRIGHT © 6/2004\e[0m "
+echo -e "               \e[1;31mReyzVPN POWERED BY GHReyz / Reyz-V4\e[0m"
+echo -e "            \e[1;31mPrivacy Policy Terms & Conditions Disclaimer\e[0m"
 echo -e  " TYPE 'menu' TO GO TO MAIN OR 'exit' TO LOGOUT" | lolcat
 echo -e  ""
 
