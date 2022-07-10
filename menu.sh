90a5cede44bd1aea22d2fcbc6b53789875538acf
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-07-10 23:50:41 +0800

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -25,16 +25,16 @@ echo -e " "
 IPVPS=$(curl -s icanhazip.com)
 DOMAIN=$(cat /etc/v2ray/domain)
 city=$(curl -s https://ipinfo.io/json | grep -o 'city": "[^"]*' | grep -o '[^"]*$')
-TIME=`curl -sS ip-api.com | grep -w "timezone" | awk '{print $3}' | cut -d'"' -f2 | tee -a /etc/afak.conf`
-ISP=`curl -sS ip-api.com | grep -w "isp" | awk '{print $3,$4,$5,$6,$7,$8,$9}' | cut -d'"' -f2 | cut -d',' -f1 | tee -a /etc/afak.conf`
-up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
-cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
-freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
-tram=$( free -m | awk 'NR==2 {print $2}' )
+TIME=$(curl -sS ip-api.com | grep -w "timezone" | awk '{print $3}' | cut -d'"' -f2 | tee -a /etc/afak.conf)
+ISP=$(curl -sS ip-api.com | grep -w "isp" | awk '{print $3,$4,$5,$6,$7,$8,$9}' | cut -d'"' -f2 | cut -d',' -f1 | tee -a /etc/afak.conf)
+up=$(uptime | awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
+cores=$(awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo)
+freq=$(awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo)
+tram=$(free -m | awk 'NR==2 {print $2}')
 cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
-cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
+cpu_usage="$((${cpu_usage1/\.*/} / ${corediilik:-1}))"
 cpu_usage+=" %"
-cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
+cname=$(awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo)
 cekxray="$(openssl x509 -dates -noout </etc/v2ray/v2ray.crt)"
 expxray=$(echo "${cekxray}" | grep 'notAfter=' | cut -f2 -d=)
 name=$(curl -sS https://raw.githubusercontent.com/GH-reyz/GH-reyz/main/Register%20IP | grep $IPVPS | awk '{print $2}')
@@ -48,38 +48,39 @@ c_vless=$(grep -oc '### [^ ]*' /etc/v2ray/vless.json | cut -d' ' -f2)
 total_xray=$(($c_xtls + $c_xvmess + $c_xvless + $c_grpc))
 total_v2ray=$(($c_vmess + $c_vless))
 total_ssh="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
-vnstat -i eth0 >/root/t1
 bulan=$(date +%b)
-today=$(vnstat -i eth0 | grep today | awk '{print $8}')
-today_v=$(vnstat -i eth0 | grep today | awk '{print $9}')
-today_rx=$(vnstat -i eth0 | grep today | awk '{print $2}')
-today_rxv=$(vnstat -i eth0 | grep today | awk '{print $3}')
-today_tx=$(vnstat -i eth0 | grep today | awk '{print $5}')
-today_txv=$(vnstat -i eth0 | grep today | awk '{print $6}')
+vnstat_profile=$(vnstat | sed -n '3p' | awk '{print $1}')
+vnstat -i ${vnstat_profile} >/root/t1
+today=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $8}')
+today_v=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $9}')
+today_rx=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $2}')
+today_rxv=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $3}')
+today_tx=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $5}')
+today_txv=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $6}')
 if [ "$(grep -wc ${bulan} /root/t1)" != '0' ]; then
   bulan=$(date +%b)
-  month=$(vnstat -i eth0 | grep "$bulan " | awk '{print $9}')
-  month_v=$(vnstat -i eth0 | grep "$bulan " | awk '{print $10}')
-  month_rx=$(vnstat -i eth0 | grep "$bulan " | awk '{print $3}')
-  month_rxv=$(vnstat -i eth0 | grep "$bulan " | awk '{print $4}')
-  month_tx=$(vnstat -i eth0 | grep "$bulan " | awk '{print $6}')
-  month_txv=$(vnstat -i eth0 | grep "$bulan " | awk '{print $7}')
+  month=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $9}')
+  month_v=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $10}')
+  month_rx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $3}')
+  month_rxv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $4}')
+  month_tx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $6}')
+  month_txv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $7}')
 else
   bulan=$(date +%Y-%m)
-  month=$(vnstat -i eth0 | grep "$bulan " | awk '{print $8}')
-  month_v=$(vnstat -i eth0 | grep "$bulan " | awk '{print $9}')
-  month_rx=$(vnstat -i eth0 | grep "$bulan " | awk '{print $2}')
-  month_rxv=$(vnstat -i eth0 | grep "$bulan " | awk '{print $3}')
-  month_tx=$(vnstat -i eth0 | grep "$bulan " | awk '{print $5}')
-  month_txv=$(vnstat -i eth0 | grep "$bulan " | awk '{print $6}')
+  month=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $8}')
+  month_v=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $9}')
+  month_rx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $2}')
+  month_rxv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $3}')
+  month_tx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $5}')
+  month_txv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $6}')
 fi
 if [ "$(grep -wc yesterday /root/t1)" != '0' ]; then
-  yesterday=$(vnstat -i eth0 | grep yesterday | awk '{print $8}')
-  yesterday_v=$(vnstat -i eth0 | grep yesterday | awk '{print $9}')
-  yesterday_rx=$(vnstat -i eth0 | grep yesterday | awk '{print $2}')
-  yesterday_rxv=$(vnstat -i eth0 | grep yesterday | awk '{print $3}')
-  yesterday_tx=$(vnstat -i eth0 | grep yesterday | awk '{print $5}')
-  yesterday_txv=$(vnstat -i eth0 | grep yesterday | awk '{print $6}')
+  yesterday=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $8}')
+  yesterday_v=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $9}')
+  yesterday_rx=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $2}')
+  yesterday_rxv=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $3}')
+  yesterday_tx=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $5}')
+  yesterday_txv=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $6}')
 else
   yesterday=NULL
   yesterday_v=NULL
@@ -91,7 +92,7 @@ fi
 rm -f /root/t1
 bash /root/.fontsam/banner
 echo -e " "
-echo -e "  Premium Script"| lolcat
+echo -e "  Premium Script" | lolcat
 echo -e " ${yellow}═════════════════════════════════════════════════════════════════"
 echo -e " ${bred1}                      • SERVER INFORMATION •                     "${NC}
 echo -e " ${yellow}═════════════════════════════════════════════════════════════════"
@@ -114,24 +115,24 @@ echo -e " ${red}OS VERSION                  : $(hostnamectl | grep "Operating Sy
 echo -e " ${red}KERNEL VERSION              : $(uname -r)${NC}"
 echo -e " ${red}EXP DATE CERT V2RAY/XRAY    : $expxray${NC}"
 echo -e " ${red}TELEGRAM                    : @GHReyz"
-echo -e " ${yellow}═════════════════════════════════════════════════════════════════" 
+echo -e " ${yellow}═════════════════════════════════════════════════════════════════"
 echo -e " TRAFFIC           TODAY          YESTERDAY          MONTH" | lolcat
-echo -e " ${white}UPLOAD            $today_tx $today_txv      $yesterday_tx $yesterday_txv         $month_tx $month_txv" 
-echo -e " DOWNLOAD          $today_rx $today_rxv      $yesterday_rx $yesterday_rxv         $month_rx $month_rxv" 
-echo -e " TOTAL             $today $today_v      $yesterday $yesterday_v         $month $month_v" 
-echo -e " ${yellow}═════════════════════════════════════════════════════════════════" 
-echo -e " ${cyan}TOTAL USER        SSH/OVPN          XRAY            V2RAY" | lolcat 
-echo -e " ${white}                     $total_ssh               $total_xray                $total_v2ray" 
-echo -e " ${yellow}═════════════════════════════════════════════════════════════════" 
-echo -e " ${bred}                         • MAIN MENU •                           "${NC}   
-echo -e " ${yellow}═════════════════════════════════════════════════════════════════" 
+echo -e " ${white}UPLOAD            $today_tx $today_txv      $yesterday_tx $yesterday_txv         $month_tx $month_txv"
+echo -e " DOWNLOAD          $today_rx $today_rxv      $yesterday_rx $yesterday_rxv         $month_rx $month_rxv"
+echo -e " TOTAL             $today $today_v      $yesterday $yesterday_v         $month $month_v"
+echo -e " ${yellow}═════════════════════════════════════════════════════════════════"
+echo -e " ${cyan}TOTAL USER        SSH/OVPN          XRAY            V2RAY" | lolcat
+echo -e " ${white}                     $total_ssh               $total_xray                $total_v2ray"
+echo -e " ${yellow}═════════════════════════════════════════════════════════════════"
+echo -e " ${bred}                         • MAIN MENU •                           "${NC}
+echo -e " ${yellow}═════════════════════════════════════════════════════════════════"
 echo -e " ${purple}[  1 ] SSH & OPENVPN                  [  2 ] WIREGUARD"
 echo -e " ${purple}[  3 ] SHADOWSOCKS R                  [  4 ] SHADOWSOCKS OBFS"
 echo -e " ${purple}[  5 ] V2RAY CORE                     [  6 ] XRAY CORE"
-echo -e " ${purple}[  7 ] TROJAN GFW                     [ 99 ] BANDWIDTH USAGE"
+echo -e " ${purple}[  7 ] TROJAN GFW"
 echo -e " ${yellow}════════════════════════════════════════════════════════════════="
-echo -e " ${bred}                        • SYSTEM MENU •                          "${NC} 
-echo -e " ${yellow}═════════════════════════════════════════════════════════════════" 
+echo -e " ${bred}                        • SYSTEM MENU •                          "${NC}
+echo -e " ${yellow}═════════════════════════════════════════════════════════════════"
 echo -e " ${purple}[  8 ] ADD/CHANGE DOMAIN VPS          [  9 ] CHANGE PORT SERVICE"
 echo -e " [ 10 ] CHANGE DNS SERVER              [ 11 ] RENEW CERTIFICATION"
 echo -e " [ 12 ] WEBMIN MENU                    [ 13 ] CHECK RAM USAGE"
@@ -139,13 +140,14 @@ echo -e " [ 14 ] REBOOT VPS                     [ 15 ] SPEEDTEST VPS"
 echo -e " [ 16 ] DISPLAY SYSTEM INFORMATION     [ 17 ] CHECK STREAM GEO"
 echo -e " [ 18 ] CHANGE SCRIPT BANNER           [ 19 ] CHECK SERVICE ERROR"
 echo -e " [ 20 ] UPDATE SCRIPT                  [  0 ] EXIT MENU${NC}"
-echo -e " ${yellow}═════════════════════════════════════════════════════════════════" 
-echo -e " ${white}🔰 Premium Script By Reyz-V4 🔰" 
-echo -e " 🔰 Thank You For Using Script By Reyz-V4 🔰" 
-echo -e " ${yellow}═════════════════════════════════════════════════════════════════" 
+echo -e " ${yellow}═════════════════════════════════════════════════════════════════"
+echo -e " ${white}🔰 Premium Script By Reyz-V4 🔰"
+echo -e " 🔰 Thank You For Using Script By Reyz-V4 🔰"
+echo -e " ${yellow}═════════════════════════════════════════════════════════════════"
 echo -e " "
 echo -e "\e[1;37m"
-read -p "     Please select an option :  " menu 
+read -p "     Please select an option :  " menu
+echo -e " "
 echo -e "\e[0m"
 case $menu in
 1)
@@ -207,9 +209,6 @@ case $menu in
   ;;
 20)
   update
-  ;;
-999)
-  vnstat
   ;;
 0)
   sleep 0.5
