ee32c1daaab0c5e00fe2d59c473c0d4b6175fa7d
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-08-31 01:00:15 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -61,47 +61,18 @@ total_xray=$(($c_xtls + $c_xvmess + $c_xvless + $c_grpc))
 total_v2ray=$(($c_vmess + $c_vless))
 total_ssh="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 
-# VNSTAT
-bulan=$(date +%b)
-vnstat_profile=$(vnstat | sed -n '3p' | awk '{print $1}' | grep -o '[^:]*')
-vnstat -i ${vnstat_profile} >/root/t1
-today=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $8}')
-today_v=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $9}')
-today_rx=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $2}')
-today_rxv=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $3}')
-today_tx=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $5}')
-today_txv=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $6}')
-if [ "$(grep -wc ${bulan} /root/t1)" != '0' ]; then
-  bulan=$(date +%b)
-  month=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $9}')
-  month_v=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $10}')
-  month_rx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $3}')
-  month_rxv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $4}')
-  month_tx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $6}')
-  month_txv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $7}')
-else
-  bulan=$(date +%Y-%m)
-  month=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $8}')
-  month_v=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $9}')
-  month_rx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $2}')
-  month_rxv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $3}')
-  month_tx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $5}')
-  month_txv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $6}')
-fi
-if [ "$(grep -wc yesterday /root/t1)" != '0' ]; then
-  yesterday=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $8}')
-  yesterday_v=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $9}')
-  yesterday_rx=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $2}')
-  yesterday_rxv=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $3}')
-  yesterday_tx=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $5}')
-  yesterday_txv=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $6}')
-else
-  yesterday=NULL
-  yesterday_v=NULL
-  yesterday_rx=NULL
-  yesterday_rxv=NULL
-  yesterday_tx=NULL
-  yesterday_txv=NULL
+#Download/Upload today
+dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
+utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
+ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload yesterday
+dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
+uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
+tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload current month
+dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
+umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
+tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
 fi
 
 rm -f /root/t1
@@ -128,25 +99,25 @@ echo -e "  \e[$text Os Version                 : $(hostnamectl | grep "Operating
 echo -e "  \e[$text Kernel Version             : $(uname -r)\e[m"
 echo -e "  \e[$text Certificate License        :${red} $expxray\e[m"
 echo -e "  \e[$line═════════════════════════════════════════════════════════════════\e[m"
-echo -e "  \e[$text Traffic           ${purple}Today          Yesterday          Month" 
-echo -e "  \e[$text Upload            $today_tx $today_txv      $yesterday_tx $yesterday_txv         $month_tx $month_txv"
-echo -e "  \e[$text Download          $today_rx $today_rxv      $yesterday_rx $yesterday_rxv         $month_rx $month_rxv"
-echo -e "  \e[$text Total             $today $today_v      $yesterday $yesterday_v         $month $month_v"
+echo -e   "  \e[33m Traffic\e[0m       \e[33mToday     Yesterday     Month   "
+echo -e   "  \e[33m Download\e[0m      $dtoday    $dyest       $dmon   \e[0m"
+echo -e   "  \e[33m Upload\e[0m        $utoday    $uyest       $umon   \e[0m"
+echo -e   "  \e[33m Total\e[0m       \033[0;36m  $ttoday    $tyest       $tmon  \e[0m "
 echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
 echo -e "  \e[$text Total User\e[m        \e[${text}Ssh/Ovpn          Xray            V2ray" 
 echo -e   "   ${white}                     $total_ssh               $total_xray                $total_v2ray"
 echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
 echo -e   " \e[$back_text                        \e[30m[\e[$box MAIN MENU\e[30m ]\e[1m                            \e[m"
 echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
-echo -e " \e[$number [  1 ]\e[m \e[$below Panel Ssh & OpenVpn\e[m           \e[$number[  6 ]\e[m \e[$red REBOOT"
+echo -e " \e[$number [  1 ]\e[m \e[$below Panel Ssh & OpenVpn\e[m           \e[$number[  6 ]\e[m \e[$below REBOOT"
 echo -e " \e[$number [  2 ]\e[m \e[$below Panel V2ray Core\e[m              \e[$number[  7 ]\e[m \e[$below Check Service Error\e[m"     
 echo -e " \e[$number [  3 ]\e[m \e[$below Panel Xray Core\e[m               \e[$number[  8 ]\e[m \e[$below System Information\e[m"
 echo -e " \e[$number [  4 ]\e[m \e[$below Panel Trojan Gfw\e[m              \e[$number[  9 ]\e[m \e[$below Menu System Script\e[m"
 echo -e " \e[$number [  5 ]\e[m \e[$below Change Banner Script\e[m          \e[$number[  0 ]\e[m \e[$below Exit Menu\e[m"
 echo -e " \e[$number [ 10 ]\e[m \e[$below Menu Themes\e[m"
 echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
-echo -e " \e[$below Client Name   : $name\e[m"
-echo -e " \e[$below Script Status : $exp\e[m"
+echo -e " \e[$below Client Name   :\e[m \e[$number $name\e[m"
+echo -e " \e[$below Script Status :\e[m \e[$number $exp\e[m"
 echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
 echo -e ""
 echo -e "\e[$below "
