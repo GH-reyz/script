0b9d96150b0ffbcfb443134b88005e47baffd548
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-08-31 01:48:37 +0800

    Update jinggo.sh

--- jinggo.sh
+++ jinggo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 red='\e[1;31m'
-green='\e[0;32m'
+green='\e[1;32m'
 blue='\e[0;34m'
 purple='\e[1;35m'
 blue_b='\e[0;94m'
@@ -10,28 +10,28 @@ NC='\e[0m'
 ar
 echo -e "\e[0m                                                                            "                                                                                                           
 echo -e "\e[93m                           AutoScript Reyz-V4                              "                                                                            
-echo -e "\e[94m             ________________________________________________              "
-echo -e "\e[94m            /                                                \             "
-echo -e "\e[94m           |    _________________________________________     |            "
-echo -e "\e[94m           |   |                                         |    |            "
-echo -e "\e[94m           |   |  root:                                  |    |            "
-echo -e "\e[94m           |   |                                         |    |            "
-echo -e "\e[94m           |   |      - Script By Reyz-V4                |    |            "
-echo -e "\e[94m           |   |      - Thanks To Allah SWT              |    |            "
-echo -e "\e[94m           |   |      - Telegram : t.me/GHReyz           |    |            "
-echo -e "\e[94m           |   |      - Thanks for who using this script |    |            "
-echo -e "\e[94m           |   |      - To EnterMenuList = 'menu'        |    |            "                                                    
-echo -e "\e[94m           |   |                                         |    |            "
-echo -e "\e[94m           |   |_________________________________________|    |            "
-echo -e "\e[94m           |                                                  |            "
-echo -e "\e[94m            \_________________________________________________/            "
-echo -e "\e[94m                   \___________________________________/                   "
-echo -e "\e[93m                           COPYRIGHT © 6/2004                              "
-echo -e "\e[93m                    ReyzVPN POWERED BY GHReyz / Reyz-V4                    "
-echo -e "\e[93m              Privacy Policy Terms & Conditions Disclaimer                 "                                                                                                
-echo -e "\e[93m        .----------------------------------------------------.             "
-echo -e "\e[93m        |             Script status : Premium                |             "
-echo -e "\e[93m        '----------------------------------------------------'             "
+echo -e "\e[1;32m             ________________________________________________              "
+echo -e "\e[1;32m            /                                                \             "
+echo -e "\e[1;32m           |    _________________________________________     |            "
+echo -e "\e[1;32m           |   |                                         |    |            "
+echo -e "\e[1;32m           |   |  root:                                  |    |            "
+echo -e "\e[1;32m           |   |                                         |    |            "
+echo -e "\e[1;32m           |   |    - Script By Reyz-V4                  |    |            "
+echo -e "\e[1;32m           |   |    - Thanks To Allah SWT                |    |            "
+echo -e "\e[1;32m           |   |    - Telegram : t.me/GHReyz             |    |            "
+echo -e "\e[1;32m           |   |    - Thanks for who using this script   |    |            "
+echo -e "\e[1;32m           |   |    - To EnterMenuList = 'menu'          |    |            "                                                    
+echo -e "\e[1;32m           |   |                                         |    |            "
+echo -e "\e[1;32m           |   |_________________________________________|    |            "
+echo -e "\e[1;32m           |                                                  |            "
+echo -e "\e[1;32m            \_________________________________________________/            "
+echo -e "\e[1;32m                   \___________________________________/                   "
+echo -e "\e[93m                             COPYRIGHT © 6/2004                              "
+echo -e "\e[93m                      ReyzVPN POWERED BY GHReyz / Reyz-V4                    "
+echo -e "\e[93m                Privacy Policy Terms & Conditions Disclaimer                 "                                                                                                
+echo -e "\e[93m          .----------------------------------------------------.             "
+echo -e "\e[93m          |             Script status : Premium                |             "
+echo -e "\e[93m          '----------------------------------------------------'             "
 echo -e  ""
 echo -e  "${red}TYPE 'menu' TO GO TO MAIN OR 'exit' TO LOGOUT"${NC}
 echo -e  ""
