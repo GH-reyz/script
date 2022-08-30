28fbac60c0080ad6026b6403085df950edd4f41f
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-08-30 22:07:20 +0800

    Add files via upload

---
+++ system.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+clear
+red='\e[1;31m'
+green='\e[1;33m'
+NC='\e[0m'
+ok='\e[0;m'
+
+echo -e  " ${red}=================================================================="
+echo -e  " ${red}                           SYSTEM MENU "                                                                                                  
+echo -e  " ${red}=================================================================="
+echo -e  " ${red}[ 1 ] ADD/CHANGE DOMAIN VPS           [ 2 ] CHANGE PORT SERVICE"
+echo -e  " [ 3 ] CHANGE DNS SERVER               [ 4 ] WEBMIN MENU"
+echo -e  " [ 5 ] CHECK GEO LOCATION              [ 6 ] CHANGE PASSWORD VPS"
+echo -e  " [ 7 ] SYSTEM INFORMATION              [ 8 ] BANDWIDTH SERVER"
+echo -e "=========================================================================="
+echo -e "${red}[ 0 ] EXIT TO MENU"
+echo -e "=========================================================================="
+echo -e "Click [ CTRL + C ] Or X For Exit"
+echo -e  "  "
+echo -e "\e[1;31m"
+read -p  "     Please select an option :  " v2raycore
+echo -e "\e[0m"
+case $v2raycore in
+   1)
+   add-host
+   ;;
+   2)
+   change
+   ;;
+   3)
+   mdns
+   ;;
+   4)
+   wbmn
+   ;;
+   6)
+   passwd
+   ;; 
+   5)
+    nf
+   ;;  
+   8)
+   vnstat
+   ;;
+   7)
+   info
+   ;;     
+   0)
+   sleep 0.5
+   clear
+   jinggo 
+    ;;
+    *)
+   echo -e "ERROR!! Please Enter an Correct Number"
+   sleep 1
+   clear
+   mv2raycore
+   ;;
+   esac
