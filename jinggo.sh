29096d61fb51b783ba2726ddc7a8a6ff4d0009b4
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-08-31 01:42:22 +0800

    Update jinggo.sh

--- jinggo.sh
+++ jinggo.sh
@@ -8,8 +8,8 @@ blue_b='\e[0;94m'
 NC='\e[0m'
 
 ar
-echo -e "\e[0m                                                                            "                                "                                                                            "
-echo -e "\e[93m                           AutoScript Reyz-V4                              "                                                                            "
+echo -e "\e[0m                                                                            "                                                                                                           
+echo -e "\e[93m                           AutoScript Reyz-V4                              "                                                                            
 echo -e "\e[94m             ________________________________________________              "
 echo -e "\e[94m            /                                                \             "
 echo -e "\e[94m           |    _________________________________________     |            "
@@ -20,7 +20,7 @@ echo -e "\e[94m           |   |      - Script By Reyz-V4                |    |  
 echo -e "\e[94m           |   |      - Thanks To Allah SWT              |    |            "
 echo -e "\e[94m           |   |      - Telegram : t.me/GHReyz           |    |            "
 echo -e "\e[94m           |   |      - Thanks for who using this script |    |            "
-echo -e "\e[94m           |   |      - To EnterMenuList = 'menu'        |    |            "                                         |    |            "
+echo -e "\e[94m           |   |      - To EnterMenuList = 'menu'        |    |            "                                                    
 echo -e "\e[94m           |   |                                         |    |            "
 echo -e "\e[94m           |   |_________________________________________|    |            "
 echo -e "\e[94m           |                                                  |            "
@@ -28,7 +28,7 @@ echo -e "\e[94m            \_________________________________________________/  
 echo -e "\e[94m                   \___________________________________/                   "
 echo -e "\e[93m                           COPYRIGHT © 6/2004                              "
 echo -e "\e[93m                    ReyzVPN POWERED BY GHReyz / Reyz-V4                    "
-echo -e "\e[93m              Privacy Policy Terms & Conditions Disclaimer                 "                                   "                                                              
+echo -e "\e[93m              Privacy Policy Terms & Conditions Disclaimer                 "                                                                                                
 echo -e "\e[93m        .----------------------------------------------------.             "
 echo -e "\e[93m        |             Script status : Premium                |             "
 echo -e "\e[93m        '----------------------------------------------------'             "
