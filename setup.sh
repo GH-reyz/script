fb070863eac4892d9646d7838ceb1a57788a0e2d
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-09-04 07:18:50 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#wget https://github.com/${GitUser}/
+GitUser="GH-reyz"
 if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
@@ -10,217 +12,252 @@ fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-
-MYIP=$(wget -qO- icanhazip.com);
-echo "Checking Vps"
-sleep 2
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo -e "\e[32mloading...\e[0m"
 clear
-
-#Welcome Note
-echo -e "============================================="
-echo -e " ${green} WELCOME TO REYZ-V4 SCRIPT VERSION 1${NC}"
-echo -e "============================================="
-sleep 2
-
-#Install Update
-echo -e "============================================="
-echo -e " ${green} UPDATE && UPGRADE PROCESS${NC}"
-echo -e "============================================="
-apt -y update 
-apt install -y bzip2 gzip coreutils screen curl
-sleep 2
-clear
-
-# Disable IPv6
-echo -e "============================================="
-echo -e " ${green} DISABLE IPV6${NC}"
-echo -e "============================================="
-sleep 2
-sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sysctl -w net.ipv6.conf.default.disable_ipv6=1
-sysctl -w net.ipv6.conf.lo.disable_ipv6=1
-echo -e "net.ipv6.conf.all.disable_ipv6 = 1
-net.ipv6.conf.default.disable_ipv6 = 1
-net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
-sleep 2
-clear
-
-# Script Access 
-MYIP=$(wget -qO- icanhazip.com);
-
-echo -e "${green}CHECKING SCRIPT ACCESS${NC}"
-sleep 2
-IZIN=$(curl https://raw.githubusercontent.com/GH-reyz/GH-reyz/main/Register%20IP | grep $MYIP | awk '{print $4}')
+# Version sc
+VERSIONSC () {
+    VCODE=V1
+    IZINVERSION=$(curl https://raw.githubusercontent.com/${GitUser}/registerv3/main/ip.conf | grep $MYIP | awk '{print $6}')
+    if [ $VCODE = $IZINVERSION ]; then
+    echo -e "\e[32mReady for script installation version 1 (websocket)..\e[0m"
+    else
+    echo -e "\e[31mYou do not have permission to install script version 1 (websocket)!\e[0m"
+    exit 0
+fi
+}
+# Valid Script
+VALIDITY () {
+    today=`date -d "0 days" +"%Y-%m-%d"`
+    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/registerv3/main/ip.conf | grep $MYIP | awk '{print $4}')
+    if [[ $today < $Exp1 ]]; then
+    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
+	VERSIONSC
+    else
+    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
+    echo -e "\e[31mPlease renew your ipvps first\e[0m"
+    exit 0
+fi
+}
+IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/registerv3/main/ip.conf | awk '{print $5}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
-    echo -e ""
-    echo -e "${green}ACCESS GRANTED...${NC}"
-    sleep 2
+echo -e "\e[32mPermission Accepted...\e[0m"
+VALIDITY
+clear
 else
-	echo -e ""
-    echo -e "${green}ACCESS DENIED...PM TELEGRAM OWNER${NC}"
-    sleep 2
-    rm -f setup.sh
-    exit 1
+echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[31mPlease buy script first\e[0m"
+rm -f setup.sh
+exit 0
 fi
 clear
-
-# Subdomain Settings
-echo -e "============================================="
-echo -e "${green}   DOMAIN INPUT${NC} "
-echo -e "============================================="
-sleep 2
-mkdir /etc/xray
-mkdir /etc/v2ray
+echo -e "\e[32mloading...\e[0m"
+clear
 mkdir /var/lib/premium-script;
+default_email=$( curl https://raw.githubusercontent.com/${GitUser}/email/main/default.conf )
 clear
+#Nama penyedia script
+echo -e "\e[1;32m════════════════════════════════════════════════════════════\e[0m"
+echo ""
+echo -e "   \e[1;32mPlease enter the name of Provider for Script."
+read -p "   Name : " nm
+echo $nm > /root/provided
+echo ""
+#Email domain
+echo -e "\e[1;32m════════════════════════════════════════════════════════════\e[0m"
 echo -e ""
-echo -e "${green}MASUKKAN DOMAIN ANDA YANG TELAH DI POINT KE IP ANDA${NC}"
-read -rp "    Enter your Domain/Host: " -e host
-ip=$(wget -qO- ipv4.icanhazip.com)
-host_ip=$(ping "${host}" -c 1 | sed '1{s/[^(]*(//;s/).*//;q}')
-if [[ ${host_ip} == "${ip}" ]]; then
-	echo -e ""
-	echo -e "${green}HOST/DOMAIN MATCHED..INSTALLATION WILL CONTINUE${NC}"
-	echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
-    echo "$host" >> /etc/v2ray/domain
-    echo "$host" >> /etc/xray/domain
-    echo "$host" > /root/domain
-	sleep 2
-	clear
+echo -e "   \e[1;32mPlease enter your email Domain/Cloudflare."
+echo -e "   \e[1;31m(Press ENTER for default email)\e[0m"
+read -p "   Email : " email
+default=${default_email}
+new_email=$email
+if [[ $email == "" ]]; then
+sts=$default_email
 else
-	echo -e "${green}HOST/DOMAIN NOT MATCHED..INSTALLATION WILL TERMINATED${NC}"
-	echo -e ""
-    rm -f setup.sh
-    exit 1
+sts=$new_email
 fi
-sleep 1
-
-# Install BBR+FQ
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p
-
+# email
+mkdir -p /usr/local/etc/xray/
+touch /usr/local/etc/xray/email
+echo $sts > /usr/local/etc/xray/email
+echo ""
+echo -e "\e[1;32m════════════════════════════════════════════════════════════\e[0m"
+echo ""
+echo -e "   .----------------------------------."
+echo -e "   |\e[1;32mPlease select a domain type below \e[0m|"
+echo -e "   '----------------------------------'"
+echo -e "     \e[1;32m1)\e[0m Enter your Subdomain"
+echo -e "     \e[1;32m2)\e[0m Use a random Subdomain"
+echo -e "   ------------------------------------"
+read -p "   Please select numbers 1-2 or Any Button(Random) : " host
+echo ""
+if [[ $host == "1" ]]; then
+echo -e "   \e[1;32mPlease enter your subdomain "
+read -p "   Subdomain: " host1
+echo "IP=" >> /var/lib/premium-script/ip.conf
+echo $host1 > /root/domain
+echo ""
+elif [[ $host == "2" ]]; then
+#install cf
+wget https://raw.githubusercontent.com/${GitUser}/scriptv3/main/install/cf.sh && chmod +x cf.sh && ./cf.sh
+rm -f /root/cf.sh
+clear
+else
+echo -e "Random Subdomain/Domain is used"
+wget https://raw.githubusercontent.com/${GitUser}/scriptv3/main/install/cf.sh && chmod +x cf.sh && ./cf.sh
+rm -f /root/cf.sh
+clear
+fi
+echo ""
+clear
+echo -e "\e[0;32mREADY FOR INSTALLATION SCRIPT...\e[0m"
+sleep 2
 #install ssh ovpn
-echo -e "============================================="
-echo -e " ${green} INSTALLING SSH && OVPN && WS ${NC}"
-echo -e "============================================="
-sleep 2
-wget https://raw.githubusercontent.com/GH-reyz/script/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-sleep 2
+echo -e "\e[0;32mINSTALLING SSH & OVPN...\e[0m"
+sleep 1
+wget https://raw.githubusercontent.com/${GitUser}/scriptv3/main/install/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+echo -e "\e[0;32mDONE INSTALLING SSH & OVPN\e[0m"
 clear
-
-#install v2ray
-echo -e "============================================="
-echo -e " ${green}  INSTALLING V2RAY${NC} "
-echo -e "============================================="
-sleep 2
-wget https://raw.githubusercontent.com/GH-reyz/script/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
-sleep 2
-clear
-
 #install Xray
-echo -e "============================================="
-echo -e " ${green} INSTALLING XRAY${NC} "
-echo -e "============================================="
-sleep 2
-wget https://raw.githubusercontent.com/GH-reyz/script/main/install-xray.sh && chmod +x install-xray.sh && screen -S v2ray ./install-xray.sh
-sleep 2
+echo -e "\e[0;32mINSTALLING XRAY CORE...\e[0m"
+sleep 1
+wget https://raw.githubusercontent.com/${GitUser}/scriptv3/main/install/ins-xray.sh && chmod +x ins-xray.sh && screen -S ins-xray ./ins-xray.sh
+echo -e "\e[0;32mDONE INSTALLING XRAY CORE\e[0m"
 clear
-
-#install ohp
-echo -e "============================================="
-echo -e " ${green}  INSTALLING OHP${NC} "
-echo -e "============================================="
-sleep 2
-wget https://raw.githubusercontent.com/GH-reyz/script/main/ohp.sh && chmod +x ohp.sh && ./ohp.sh
-sleep 2
+#install Trojan GO
+echo -e "\e[0;32mINSTALLING TROJAN GO...\e[0m"
+sleep 1
+wget https://raw.githubusercontent.com/${GitUser}/scriptv3/main/install/trojan-go.sh && chmod +x trojan-go.sh && screen -S trojan-go ./trojan-go.sh
+echo -e "\e[0;32mDONE INSTALLING TROJAN GO\e[0m"
 clear
-
+#install SET-BR
+echo -e "\e[0;32mINSTALLING SET-BR...\e[0m"
+sleep 1
+wget https://raw.githubusercontent.com/${GitUser}/scriptv3/main/install/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+echo -e "\e[0;32mDONE INSTALLING SET-BR...\e[0m"
+clear
+#install ohp-server
+echo -e "\e[0;32mINSTALLING OHP PORT...\e[0m"
+sleep 1
+wget https://raw.githubusercontent.com/${GitUser}/scriptv3/main/install/ohp.sh && chmod +x ohp.sh && ./ohp.sh
+wget https://raw.githubusercontent.com/${GitUser}/scriptv3/main/install/ohp-dropbear.sh && chmod +x ohp-dropbear.sh && ./ohp-dropbear.sh
+wget https://raw.githubusercontent.com/${GitUser}/scriptv3/main/install/ohp-ssh.sh && chmod +x ohp-ssh.sh && ./ohp-ssh.sh
+echo -e "\e[0;32mDONE INSTALLING OHP PORT\e[0m"
+clear
+#install websocket
+echo -e "\e[0;32mINSTALLING WEBSOCKET PORT...\e[0m"
+wget https://raw.githubusercontent.com/${GitUser}/scriptv3/main/websocket-python/websocket.sh && chmod +x websocket.sh && screen -S websocket.sh ./websocket.sh
+echo -e "\e[0;32mDONE INSTALLING WEBSOCKET PORT\e[0m"
+clear
+# set time GMT +8
+ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
+# install clouflare JQ
+apt install jq curl -y
+# install webserver
+apt -y install nginx
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/${GitUser}/scriptv3/main/nginx.conf"
+mkdir -p /home/vps/public_html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/${GitUser}/scriptv3/main/vps.conf"
+/etc/init.d/nginx restart
+#finish
 rm -f /root/ssh-vpn.sh
-rm -f /root/ins-vt.sh
-rm -f /root/install-xray.sh
+rm -f /root/ins-xray.sh
+rm -f /root/trojan-go.sh
+rm -f /root/set-br.sh
 rm -f /root/ohp.sh
+rm -f /root/ohp-dropbear.sh
+rm -f /root/ohp-ssh.sh
+rm -f /root/websocket.sh
 # Colour Default
 echo "1;36m" > /etc/banner
 echo "30m" > /etc/box
-echo "0;33m" > /etc/line
-echo "0;32m" > /etc/text
-echo "0;33m" > /etc/below
+echo "1;31m" > /etc/line
+echo "1;32m" > /etc/text
+echo "1;33m" > /etc/below
 echo "47m" > /etc/back
-echo "0;32m" > /etc/number
+echo "1;35m" > /etc/number
 echo 3d > /usr/bin/test
-
-apt install lolcat -y
-apt install figlet -y
-
-wget -q -O /usr/bin/banner_changer "https://raw.githubusercontent.com/GH-reyz/script/main/banner_changer.sh" && chmod +x /usr/bin/banner_changer
-[[ ! -d /root/.fontsam ]] && {
-    mkdir /root/.fontsam
-    touch /root/.fontsam/fontlist
-    touch /root/.fontsam/count
-    curl -Ss https://raw.githubusercontent.com/syfqsamvpn/font/main/list >/root/.fontsam/fontlist
-    echo "figlet -f slant Reyz-V4 -c | lolcat" >/root/.fontsam/banner
-    echo "Reyz-V4" >/root/.fontsam/bannername
-}
-
-#install resolv
-apt install resolvconf
-systemctl start resolvconf.service
-systemctl enable resolvconf.service
-echo 'nameserver 8.8.8.8' > /etc/resolvconf/resolv.conf.d/head
-echo 'nameserver 8.8.8.8' > /etc/resolv.conf
-systemctl restart resolvconf.service
-
+# Version
+ver=$( curl https://raw.githubusercontent.com/${GitUser}/scriptv3/main/ver.conf )
+history -c
+echo "$ver" > /home/ver
 clear
 echo " "
-echo "Installation Completed!!"
+echo "Installation has been completed!!"
 echo " "
-echo "========================= REYZ-V4 AUTOSCRIPT VERSION 1 ====================" | tee -a log-install.txt
+echo "=========================[ REYZVPN AUTOSCRIPT V3 ]========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
+echo "-----------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI SSH & OpenVPN]" | tee -a log-install.txt
+echo "    -------------------------" | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
-echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4                : 444, 777"  | tee -a log-install.txt
-echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
-echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194, UDP 2200"  | tee -a log-install.txt
+echo "   - OpenVPN SSL             : 110"  | tee -a log-install.txt
+echo "   - Stunnel4                : 222, 777"  | tee -a log-install.txt
+echo "   - Dropbear                : 442, 109"  | tee -a log-install.txt
+echo "   - OHP Dropbear            : 8585"  | tee -a log-install.txt
+echo "   - OHP SSH                 : 8686"  | tee -a log-install.txt
+echo "   - OHP OpenVPN             : 8787"  | tee -a log-install.txt
+echo "   - Websocket SSH(HTTP)     : 2081"  | tee -a log-install.txt
+echo "   - Websocket SSL(HTTPS)    : 222"  | tee -a log-install.txt
+echo "   - Websocket OpenVPN       : 2084"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI Sqd, Bdvp, Ngnx]" | tee -a log-install.txt
+echo "    ---------------------------" | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8000 (limit to IP Server)"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                   : 81"  | tee -a log-install.txt
-echo "   - SSH WS/OVPN WS          : 2082, 2095"  | tee -a log-install.txt
-echo "   - DROPBEAR OHP            : 8010"  | tee -a log-install.txt
-echo "   - OPENVPN OHP             : 8111"  | tee -a log-install.txt
-echo "   - V2RAY Vmess TLS         : 5443"  | tee -a log-install.txt
-echo "   - V2RAY Vmess None TLS    : 6443"  | tee -a log-install.txt
-echo "   - V2RAY Vless TLS         : 7443"  | tee -a log-install.txt
-echo "   - V2RAY Vless None TLS    : 8443"  | tee -a log-install.txt
-echo "   - Trojan                  : 9443"  | tee -a log-install.txt
-echo "   - XRAY VMESS TLS          : 2929"  | tee -a log-install.txt
-echo "   - XRAY VMESS NON TLS      : 3939"  | tee -a log-install.txt
-echo "   - XRAY VLESS TLS          : 4949"  | tee -a log-install.txt
-echo "   - XRAY VLESS NON TLS      : 5959"  | tee -a log-install.txt
-echo "   - XRAY VLESS XTLS         : 443"  | tee -a log-install.txt
-echo "   - XRAY VLESS GRPC         : 6969"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI XRAY]"  | tee -a log-install.txt
+echo "    ----------------" | tee -a log-install.txt
+echo "   - Xray Vmess Ws Tls       : 443"  | tee -a log-install.txt
+echo "   - Xray Vless Ws Tls       : 443"  | tee -a log-install.txt
+echo "   - Xray Vless Tcp Xtls     : 443"  | tee -a log-install.txt
+echo "   - Xray Vmess Ws None Tls  : 80"  | tee -a log-install.txt
+echo "   - Xray Vless Ws None Tls  : 8080"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI TROJAN]"  | tee -a log-install.txt
+echo "    ------------------" | tee -a log-install.txt
+echo "   - Xray Trojan Tcp Tls     : 443"  | tee -a log-install.txt
+echo "   - Trojan Go               : 2083"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI CLASH FOR ANDROID (YAML)]"  | tee -a log-install.txt
+echo "    -----------------------------------" | tee -a log-install.txt
+echo "   - Xray Vmess Ws Yaml      : Yes"  | tee -a log-install.txt
+echo "   - Shadowsocks Yaml        : Yes"  | tee -a log-install.txt
+echo "   - ShadowsocksR Yaml       : Yes"  | tee -a log-install.txt
+echo "   --------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/MALAYSIA (GMT +8)"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Kuala_Lumpur (GMT +8)"  | tee -a log-install.txt
 echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
 echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
 echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
 echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
 echo "   - Autoreboot On 05.00 GMT +8" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
 echo "   - Auto Delete Expired Account" | tee -a log-install.txt
 echo "   - Full Orders For Various Services" | tee -a log-install.txt
 echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   - Dev/Main                : Horas/MD"  | tee -a log-install.txt
-echo "   - Modded by               : GHReyz / ReyzV4"  | tee -a log-install.txt
-echo "   - Telegram                : t.me/GHReyz"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "=========================== SCRIPT BY REYZ-V4 =========================" | tee -a log-install.txt
+echo "-------------------------SCRIPT BY REYZVPN-----------------------" | tee -a log-install.txt
+clear
 echo ""
-sleep 1
-rm -f setup.sh
-read -n 1 -r -s -p $'Press any key to reboot...\n';reboot
+echo ""
+echo -e "    \e[1;32m.------------------------------------------.\e[0m"
+echo -e "    \e[1;32m|     SUCCESFULLY INSTALLED THE SCRIPT     |\e[0m"
+echo -e "    \e[1;32m'------------------------------------------'\e[0m"
+echo ""
+echo -e "   \e[1;32mYour VPS Will Be Automatical Reboot In 5 seconds\e[0m"
+rm -r setup.sh
+sleep 5
+reboot
