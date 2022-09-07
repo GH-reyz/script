064a4fd406adfe8faf7cc950fe2499653f16b8c6
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-09-07 16:54:25 +0800

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -1,81 +1,36 @@
 #!/bin/bash
-#wget https://github.com/${GitUser}/
-GitUser="GH-reyz"
-#IZIN SCRIPT
-MYIP=$(curl -sS ipv4.icanhazip.com)
-echo -e "\e[32mloading...\e[0m"
 clear
-if [[ "$IP" = "" ]]; then
-domain=$(cat /usr/local/etc/xray/domain)
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com)
+IZIN=$(curl https://raw.githubusercontent.com/GH-reyz/GH-reyz/main/Register%20IP | grep $MYIP | awk '{print $4}')
+if [ $MYIP = $IZIN ]; then
+  echo -e ""
+  clear
 else
-domain=$IP
+  echo -e ""
+  echo -e "${green}ACCESS DENIED...PM TELEGRAM OWNER @GHReyz${NC}"
+  exit 1
 fi
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-CITY=$(curl -s ipinfo.io/city )
-WKT=$(date +"%T")
-IPVPS=$(curl -s ipinfo.io/ip )
-cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
-cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
-freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
-tram=$( free -m | awk 'NR==2 {print $2}' )
-swap=$( free -m | awk 'NR==4 {print $2}' )
-DATE=$(date +"%d-%B-%Y")
-red=='\e[0;31m'
-#Download/Upload today
-dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
-utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
-ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
-#Download/Upload yesterday
-dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
-uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
-tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
-#Download/Upload current month
-dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
-umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
-tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
-clear
-# OS Uptime
-uptime="$(uptime -p | cut -d " " -f 2-10)"
-# USERNAME
-rm -f /usr/bin/user
-username=$( curl https://raw.githubusercontent.com/${GitUser}/registerv3/main/ip.conf | grep $MYIP | awk '{print $2}' )
-echo "$username" > /usr/bin/user
-# Order ID
-rm -f /usr/bin/ver
-user=$( curl https://raw.githubusercontent.com/${GitUser}/registerv3/main/ip.conf | grep $MYIP | awk '{print $3}' )
-echo "$user" > /usr/bin/ver
-# validity
-rm -f /usr/bin/e
-valid=$( curl https://raw.githubusercontent.com/${GitUser}/registerv3/main/ip.conf | grep $MYIP | awk '{print $4}' )
-echo "$valid" > /usr/bin/e
-# DETAIL ORDER
-username=$(cat /usr/bin/user)
-oid=$(cat /usr/bin/ver)
-exp=$(cat /usr/bin/e)
-clear
-
-# STATUS EXPIRED ACTIVE
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[4$below" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}(Active)${Font_color_suffix}"
-Error="${Green_font_prefix}${Font_color_suffix}${Red_font_prefix}[EXPIRED]${Font_color_suffix}"
-
-today=`date -d "0 days" +"%Y-%m-%d"`
-Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/registerv3/main/ip.conf | grep $MYIP | awk '{print $4}')
-if [[ $today < $Exp1 ]]; then
-sts="${Info}"
-else
-sts="${Error}"
-fi
-echo -e "\e[32mloading...\e[0m"
-clear
-# CERTIFICATE STATUS
-d1=$(date -d "$valid" +%s)
-d2=$(date -d "$today" +%s)
-certifacate=$(( (d1 - d2) / 86400 ))
-# PROVIDED
-creditt=$(cat /root/provided)
-# BANNER COLOUR
-banner_colour=$(cat /etc/banner)
+echo -e " "
+# SYSTEM INFO
+IPVPS=$(curl -s icanhazip.com)
+DOMAIN=$(cat /etc/v2ray/domain)
+city=$(curl -s https://ipinfo.io/json | grep -o 'city": "[^"]*' | grep -o '[^"]*$')
+TIME=$(curl -sS ip-api.com | grep -w "timezone" | awk '{print $3}' | cut -d'"' -f2 | tee -a /etc/afak.conf)
+ISP=$(curl -sS ip-api.com | grep -w "isp" | awk '{print $3,$4,$5,$6,$7,$8,$9}' | cut -d'"' -f2 | cut -d',' -f1 | tee -a /etc/afak.conf)
+up=$(uptime | awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
+cores=$(awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo)
+freq=$(awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo)
+tram=$(free -m | awk 'NR==2 {print $2}')
+cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
+cpu_usage="$((${cpu_usage1/\.*/} / ${corediilik:-1}))"
+cpu_usage+=" %"
+cname=$(awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo)
+tram=$(free -m | awk 'NR==2 {print $2}')
+uram=$(free -m | awk 'NR==2 {print $3}')
+fram=$(free -m | awk 'NR==2 {print $4}')
+red='\e[0;31m'
+white='\e[0;37m'
 # TEXT ON BOX COLOUR
 box=$(cat /etc/box)
 # LINE COLOUR
@@ -88,101 +43,157 @@ below=$(cat /etc/below)
 back_text=$(cat /etc/back)
 # NUMBER COLOUR
 number=$(cat /etc/number)
-# BANNER
-banner=$(cat /usr/bin/bannerku)
-ascii=$(cat /usr/bin/test)
-clear
-echo -e "\e[$banner_colour"
-figlet -f $ascii "$banner"
-echo -e "\e[$text Premium-Script-By-ReyzV4 "
-echo -e   " \e[$line------------------------------------------------------------\e[m"
-echo -e   " \e[$back_text                    \e[30m[\e[$box SERVER INFORMATION\e[30m ]\e[1m                  \e[m"
-echo -e   " \e[$line------------------------------------------------------------\e[m"
-echo -e "  \e[$text Cpu Model            :$cname"
-echo -e "  \e[$text Number Of Core       : $cores"
-echo -e "  \e[$text Cpu Frequency        :$freq MHz"
-echo -e "  \e[$text Total Amount Of Ram  : $tram MB"
-echo -e "  \e[$text System Uptime        : $uptime"
-echo -e "  \e[$text Isp/Provider Name    : $ISP"
-echo -e "  \e[$text City Location        : $CITY"
-echo -e "  \e[$text Time Location        : $WKT"
-echo -e "  \e[$text Date Location        : $DATE"
-echo -e "  \e[$text Ip Vps/Address       : $IPVPS"
-echo -e "  \e[$text Domain Name          : $domain\e[0m"
-echo -e "  \e[$text Telegram             : @GHReyz"
-echo -e "  \e[$text Script Version       : SC (V3)"
-echo -e "  \e[$text Order ID             : $oid"
-echo -e "  \e[$text Certificate Status   :\e[m${red}Expired in $certifacate days"
-echo -e "  \e[$text Provided By          : $creditt"
-echo -e   " \e[$line------------------------------------------------------------\e[m"
-echo -e   "  \e[33m Traffic\e[0m       \e[33mToday     Yesterday     Month   "
-echo -e   "  \e[33m Download\e[0m      $dtoday    $dyest       $dmon   \e[0m"
-echo -e   "  \e[33m Upload\e[0m        $utoday    $uyest       $umon   \e[0m"
-echo -e   "  \e[33m Total\e[0m       \033[0;36m  $ttoday    $tyest       $tmon  \e[0m "
-echo -e   " \e[$line------------------------------------------------------------\e[m"
-echo -e   " \e[$back_text                        \e[30m[\e[$box MAIN MENU\e[30m ]\e[1m                       \e[m"
-echo -e   " \e[$line------------------------------------------------------------\e[m"
-echo -e   "  \e[$number [•1]\e[m \e[$below XRAY VMESS & VLESS\e[m          \e[$number [•7]\e[m \e[$below MENU THEMES\e[m"
-echo -e   "  \e[$number [•2]\e[m \e[$below TROJAN XRAY & GO\e[m            \e[$number [•8]\e[m \e[$below CLEAR LOG VPS\e[m"
-echo -e   "  \e[$number [•3]\e[m \e[$below PANEL WIREGUARDS\e[m            \e[$number [•9]\e[m \e[$below CHANGE PORT\e[m"
-echo -e   "  \e[$number [•4]\e[m \e[$below PANEL SHADOWSOCKS\e[m           \e[$number [10]\e[m \e[$below CHECK RUNNING\e[m"
-echo -e   "  \e[$number [•5]\e[m \e[$below OPENSSH & OPENVPN\e[m           \e[$number [11]\e[m \e[$below REBOOT VPS\e[m"
-echo -e   "  \e[$number [•6]\e[m \e[$below SYSTEM MENU\e[m                 \e[$number [12]\e[m \e[$below INFO ALL PORT\e[m"
-echo -e   " \e[$line-----------------------------------------------------------\e[m"
-echo -e   "  \e[$number Client Name   :\e[m \e[$below $username\e[m"
-echo -e   "  \e[$number Script Status :\e[m \e[$below $exp\e[m $sts"
-echo -e   " \e[$line-----------------------------------------------------------\e[m"
-echo -e   ""
-echo -e   "  \e[$below [Ctrl + C] For exit from main menu\e[m"
-echo -e   "\e[$below "
-read -p   "   Select From Options [1-12 or x] :  " menu
-echo -e   ""
+
+# CERT V2RAY/XRAY
+cekxray="$(openssl x509 -dates -noout </etc/v2ray/v2ray.crt)"
+expxray=$(echo "${cekxray}" | grep 'notAfter=' | cut -f2 -d=)
+
+# CLIENT INFOMATION
+name=$(curl -sS https://raw.githubusercontent.com/GH-reyz/GH-reyz/main/Register%20IP | grep $IPVPS | awk '{print $2}')
+exp=$(curl -sS https://raw.githubusercontent.com/GH-reyz/GH-reyz/main/Register%20IP | grep $IPVPS | awk '{print $3}')
+
+# USER TOTAL
+c_xtls=$(grep -oc '### [^ ]*' /etc/xray/vless-direct.json | cut -d' ' -f2)
+c_xvmess=$(grep -oc '### [^ ]*' /etc/xray/v2ray-tls.json | cut -d' ' -f2)
+c_xvless=$(grep -oc '### [^ ]*' /etc/xray/vless-tls.json | cut -d' ' -f2)
+c_grpc=$(grep -oc '### [^ ]*' /etc/xray/vless-grpc.json | cut -d' ' -f2)
+c_vmess=$(grep -oc '### [^ ]*' /etc/v2ray/config.json | cut -d' ' -f2)
+c_vless=$(grep -oc '### [^ ]*' /etc/v2ray/vless.json | cut -d' ' -f2)
+total_xray=$(($c_xtls + $c_xvmess + $c_xvless + $c_grpc))
+total_v2ray=$(($c_vmess + $c_vless))
+total_ssh="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+
+# VNSTAT
+bulan=$(date +%b)
+vnstat_profile=$(vnstat | sed -n '3p' | awk '{print $1}' | grep -o '[^:]*')
+vnstat -i ${vnstat_profile} >/root/t1
+today=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $8}')
+today_v=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $9}')
+today_rx=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $2}')
+today_rxv=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $3}')
+today_tx=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $5}')
+today_txv=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $6}')
+if [ "$(grep -wc ${bulan} /root/t1)" != '0' ]; then
+  bulan=$(date +%b)
+  month=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $9}')
+  month_v=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $10}')
+  month_rx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $3}')
+  month_rxv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $4}')
+  month_tx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $6}')
+  month_txv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $7}')
+else
+  bulan=$(date +%Y-%m)
+  month=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $8}')
+  month_v=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $9}')
+  month_rx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $2}')
+  month_rxv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $3}')
+  month_tx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $5}')
+  month_txv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $6}')
+fi
+if [ "$(grep -wc yesterday /root/t1)" != '0' ]; then
+  yesterday=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $8}')
+  yesterday_v=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $9}')
+  yesterday_rx=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $2}')
+  yesterday_rxv=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $3}')
+  yesterday_tx=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $5}')
+  yesterday_txv=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $6}')
+else
+  yesterday=NULL
+  yesterday_v=NULL
+  yesterday_rx=NULL
+  yesterday_rxv=NULL
+  yesterday_tx=NULL
+  yesterday_txv=NULL
+fi
+
+rm -f /root/t1
+bash /root/.fontsam/banner
+echo -e "  Premium Script" | lolcat
+echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
+echo -e   " \e[$back_text                    \e[30m[\e[$box SERVER INFORMATION\e[30m ]\e[1m                       \e[m"
+echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
+echo -e "  \e[$text Cpu Model                  :$cname\e[m"
+echo -e "  \e[$text Number Of Core             : $cores\e[m"
+echo -e "  \e[$text Cpu Usage                  : $cpu_usage1 %\e[m"
+echo -e "  \e[$text Cpu Frequency              :$freq MHz\e[m"
+echo -e "  \e[$text Total Amount Of Ram        : $tram MB\e[m"
+echo -e "  \e[$text Free Ram                   : $fram MB\e[m"
+echo -e "  \e[$text System Uptime              :$up\e[m"
+echo -e "  \e[$text Isp/Provider Name          : $ISP\e[m"
+echo -e "  \e[$text City Location              : $city\e[m"
+echo -e "  \e[$text Time Location              : $TIME\e[m"
+echo -e "  \e[$text IpVps Number               : $IPVPS\e[m"
+echo -e "  \e[$text Domain Name                : $DOMAIN\e[m"
+echo -e "  \e[$text Telegram                   : @GHReyz\e[m"
+echo -e "  \e[$text Script Version             : REYZ-V4 (V1)\e[m"
+echo -e "  \e[$text Os Version                 : $(hostnamectl | grep "Operating System" | cut -d ' ' -f5-)\e[m"
+echo -e "  \e[$text Kernel Version             : $(uname -r)\e[m"
+echo -e "  \e[$text Certificate License        :\e[m \e[$red$expxray\e[m"
+echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
+echo -e "  \e[$text Traffic\e[m           \e[$below Today          Yesterday          Month" 
+echo -e "  \e[$text Upload\e[m            \e[$white $today_tx $today_txv      $yesterday_tx $yesterday_txv         $month_tx $month_txv"
+echo -e "  \e[$text Download\e[m          \e[$white $today_rx $today_rxv      $yesterday_rx $yesterday_rxv         $month_rx $month_rxv"
+echo -e "  \e[$text Total\e[m             \e[$white $today $today_v      $yesterday $yesterday_v         $month $month_v"
+echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
+echo -e "  \e[$text Total User\e[m        \e[$below Ssh/Ovpn          Xray            V2ray" 
+echo -e   "                        \e[$white $total_ssh               $total_xray                $total_v2ray"
+echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
+echo -e   " \e[$back_text                        \e[30m[\e[$box MAIN MENU\e[30m ]\e[1m                            \e[m"
+echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
+echo -e " \e[$number [  1 ]\e[m \e[$below Panel Ssh & OpenVpn\e[m           \e[$number[  6 ]\e[m \e[$red REBOOT"
+echo -e " \e[$number [  2 ]\e[m \e[$below Panel V2ray Core\e[m              \e[$number[  7 ]\e[m \e[$below Check Service Error\e[m"     
+echo -e " \e[$number [  3 ]\e[m \e[$below Panel Xray Core\e[m               \e[$number[  8 ]\e[m \e[$below System Information\e[m"
+echo -e " \e[$number [  4 ]\e[m \e[$below Panel Trojan Gfw\e[m              \e[$number[  9 ]\e[m \e[$below Menu System Script\e[m"
+echo -e " \e[$number [  5 ]\e[m \e[$below Change Banner Script\e[m          \e[$number[ 10 ]\e[m \e[$below Menu Theme\e[m"
+echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
+echo -e " \e[$below Client Name   :\e[m \e[$number $name\e[m"
+echo -e " \e[$below Script Status :\e[m \e[$red $exp\e[m"
+echo -e   " \e[$line═════════════════════════════════════════════════════════════════\e[m"
+echo -e ""
+echo -e "\e[$white "
+read -p "     Please select an option :  " menu
+echo -e " "
+echo -e "\e[0m"
 case $menu in
 1)
-xraay
-;;
+  mssh
+  ;;
 2)
-trojaan
-;;
+  mv2raycore
+  ;;
 3)
-wgr
-;;
+  mxraycore
+  ;;
 4)
-ssssr
-;;
+  mtrojan
+  ;;
 5)
-ssh
-;;
+  banner_changer
+  ;;
 6)
-system
-;;
+  reboot
+  ;;
 7)
-themes
-;;
+  checksystem
+  ;;
 8)
-clear-log
-;;
+  info
+  ;;
 9)
-change-port
-;;
+  system
+  ;;
 10)
-check-sc
-;;
-11)
-reboot
-;;
-12)
-info
-;;
-x)
-clear
-exit
-echo  -e "\e[1;31mPlease Type menu For More Option, Thank You\e[0m"
-;;
+  themes
+  ;;
+0)
+  sleep 0.5
+  clear
+  jinggo
+  ;;
 *)
-clear
-echo  -e "\e[1;31mPlease enter an correct number\e[0m"
-sleep 1
-menu
-;;
+  echo -e "ERROR!! Please Enter an Correct Number"
+  sleep 1
+  clear
+  menu
+  ;;
 esac
